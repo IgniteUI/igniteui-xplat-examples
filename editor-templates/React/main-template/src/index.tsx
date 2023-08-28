5ca58af20cf17c3c35ee34a51ed7f21e17711089
Deyan Kamburov <dkamburov@infragistics.com>
2023-08-28 18:09:56 +0300

    apply review notes

--- editor-templates/React/main-template/src/index.tsx
+++ editor-templates/React/main-template/src/index.tsx
@@ -19,10 +19,11 @@ import './index.css';
 
 //ifdef webgrids
 import 'igniteui-react-grids/grids/combined';
+import 'igniteui-react-grids/grids/themes/light/bootstrap.css';
 //endifdef webgrids
 
 //ifdef editor
-import 'igniteui-react-grids/grids/themes/light/bootstrap.css';
+import 'igniteui-webcomponents/themes/light/bootstrap.css';
 import { defineAllComponents } from 'igniteui-webcomponents';
 //endifdef editor
 
