702a72e7e471bfa304dc887908f5d5b94d7e2379
HUSSAR-mtrela (Martin Trela) <HUSSAR-mtrela@users.noreply.github.com>
2023-09-01 15:37:13 -0400

    add new data sources and data scripts (#285)
    * add code-gen-tools
    
    * add more data sources and todo
    
    * added stock makret data
    
    * add stock data
    
    * add fetch data scripts
    
    * generate stocks 10-2000

--- code-gen-tools/gulpfile.js
+++ code-gen-tools/gulpfile.js
@@ -4,11 +4,12 @@ let es = require('event-stream');
 let fs = require('fs.extra');
 let path = require('path');
 let utils = require('./utils.js')
+let request = require('request');
 
 function log(msg) {
     console.log('>> ' + msg);
 }
-console.log('loaded');
+// console.log('loaded');
 
 let CodeGenLib = "../code-gen-library";
 
@@ -66,6 +67,7 @@ function toJSON(cb) {
     let outputLines = [];
     for (let i = 0; i < lines.length; i++) {
         let line = lines[i];
+        // skip comments
         if (line.indexOf("/*") >= 0) continue;
         if (line.indexOf("//") >= 0) continue;
 
@@ -390,8 +392,60 @@ function convertColumnTypes(cb) {
 }
 exports.convertColumnTypes = convertColumnTypes;
 
+exports.convertWorldStats = function convertWorldStats(cb) {
 
-exports.convertCSV = function convertCSV(cb) {
+    let csvName = 'world_countries_all'
+    let csvPath = '../convert/' + csvName + '.csv';
+    csvData = fs.readFileSync(csvPath, "utf8");
+
+    let jsonData = [];
+    let csvLines = csvData.split('\r\n');
+    let csvColumns = csvLines[0].split(',');
+
+    for (let i = 1; i < csvLines.length; i++) {
+        const csvRow = csvLines[i].split(',');
+        if (csvRow === undefined || csvRow.length <= 1) {
+            continue;
+        }
+
+        let jsonItem = {};
+        for (let c = 0; c < csvColumns.length; c++) {
+            const column = csvColumns[c];
+            if (column === "") continue;
+
+            let value = csvRow[c];
+            if (value === undefined) {
+                console.log("ERROR: undefined value at " + i + " row and col: " + column )
+                continue;
+            }
+            value = value.trim();
+
+            let num = utils.strToNumber(value);
+            if (num !== null) {
+                jsonItem[column] = num;
+            } else {
+                jsonItem[column] = value;
+            }
+        }
+
+        if (jsonItem.Population > 1000) {
+            jsonItem["GDPPerPerson"] = Math.round(jsonItem["GDPTotal"] * 1000000 / jsonItem["Population"]);
+            jsonData.push(jsonItem);
+        }
+
+    }
+    jsonData.sort((a, b) => a.Population < b.Population ? 1 : -1);
+
+    for (let i = 0; i < jsonData.length; i++) {
+        jsonData[i].Rank = i + 1;
+    }
+
+    let jsonPath = CodeGenLib + "/WorldStats/XPLAT.json";
+    saveJSON(jsonPath, jsonData,  "compact");
+    cb();
+}
+
+exports.convertStockSP500 = function convertStockSP500(cb) {
     // let csvName = 'StockSP500Prices'
     let csvName = 'StockSP500Cap'
     let csvPath = '../convert/' + csvName + '.csv';
@@ -404,7 +458,7 @@ exports.convertCSV = function convertCSV(cb) {
     // console.log('csvLines ' + csvLines.length)
     let csvColumns = csvLines[0].split(',');
 
-    let min = 100000000000000000;
+    // let min = 100000000000000000;
     // console.log(csvColumns)
     for (let i = 1; i < csvLines.length; i++) {
         const csvRow = csvLines[i].split(',');
@@ -479,5 +533,427 @@ exports.convertCSV = function convertCSV(cb) {
 
     let jsonPath = CodeGenLib + "/" + csvName + "/XPLAT.json";
     saveJSON(jsonPath, jsonData,  "compact");
+    cb();
+}
+
+exports.convertStockMarket = function convertStockMarket(cb) {
+
+    let csvMarketCaps = utils.readCSV('../convert/market-capitalization.csv');
+    // let csvSectors = ;
+    // console.log('csvMarketCaps ');
+    // console.log(  csvMarketCaps.data);
+
+    let companiesALL = [];
+
+    let csvMappings = [
+        utils.hash('Symbol', utils.readCSV('../convert/market-sectors-yahoo.csv').data),
+        // utils.hash('Symbol', utils.readCSV('../convert/market-sectors.csv').data),
+        utils.hash('Symbol', utils.readCSV('../convert/market-earnings.csv').data),
+        utils.hash('Symbol', utils.readCSV('../convert/market-employees.csv').data),
+        utils.hash('Symbol', utils.readCSV('../convert/market-operating-margin.csv').data),
+        utils.hash('Symbol', utils.readCSV('../convert/market-revenue.csv').data),
+        utils.hash('Symbol', utils.readCSV('../convert/market-PE-ratio.csv').data),
+    ];
+
+    for (let i = 0; i < csvMarketCaps.data.length; i++) {
+        let company = csvMarketCaps.data[i];
+        for (const csv of csvMappings) {
+            let lookup = csv[company.Symbol];
+            if (lookup !== undefined) {
+                let lookupColumns = Object.keys(lookup);
+                for (const column of lookupColumns) {
+                    if (company[column]) continue;
+                    company[column] = lookup[column];
+                }
+            }
+        }
+
+        company.Country = undefined;
+        company.Rank = undefined;
+
+        if (company.MarketCap > 0 && company.Price > 0) {
+            company.Shares = Math.round(company.MarketCap / company.Price);
+        } else {
+            company.Shares = 0;
+        }
+
+        if (company.Earnings > 0 && company.Shares > 0) {
+            company.EarningsPerShare = Math.round(company.Earnings / company.Shares * 100) / 100;
+        } else {
+            company.EarningsPerShare = 0;
+        }
+
+        if (company.Revenue > 0 && company.Shares > 0) {
+            company.RevenuePerShare = Math.round(company.Revenue / company.Shares * 100) / 100;
+        } else {
+            company.RevenuePerShare = 0;
+        }
+
+        // if (company.Sector) {
+
+            companiesALL.push(company);
+        // }
+        // if (i > 10) {
+        //     break;
+        // }
+    }
+
+    companiesALL = companiesALL.sort((a, b) => a.MarketCap < b.MarketCap ? 1 : -1);
+
+    var missing = [];
+    let rank = 1;
+    for (const company of companiesALL) {
+        company.Rank = rank++;
+        // console.log(utils.toStringLine(company));
+
+            if (company.Sector === undefined) {
+            // console.log(symbol);
+            missing.push(company.Symbol);
+        }
+    }
+    // console.log(missing.join(', '));
+    // console.log('missingSymbols=' + missing.length);
+    // console.log('companiesALL=' + companiesALL.length);
+
+    function saveCompanies(number) {
+        let data = utils.splice(companiesALL, 0, number);
+        let missingSector = []
+        for (const item of data) {
+             if (item.Sector === undefined)
+                missingSector.push('500,CompanyName,' + item.Symbol + ',$0.0 M,United States,');
+                // missingSector.push(item.Symbol);
+        }
+
+        console.log(missingSector.join('\n'));
+        console.log('missingSymbols=' + missingSector.length);
+
+        let path = CodeGenLib + "/StockMarket" + number + "/XPLAT.json";
+        saveJSON(path, data,  "compact");
+
+    }
+    saveCompanies( 2000);
+    saveCompanies( 1000);
+    saveCompanies(  500);
+    saveCompanies(  100);
+    saveCompanies(  10);
+
+    // console.log( 'companiesALL ' )
+    // console.log(companiesALL);
+    // console.log(stocks);
+
+    cb();
+}
+
+function getStockSymbols(limit) {
+    if (limit === undefined) limit = 10000;
+
+    // let CSV = utils.readCSV('../convert/sp500.csv');
+    let CSV = utils.readCSV('../convert/market-capitalization.csv');
+
+    // var stockSymbols = ['IBM', 'TSLA', 'MSFT', ];
+    var stockSymbols = [];
+    for (let i = 0; i < CSV.data.length; i++) {
+        let company = CSV.data[i];
+        let symbol = company.Symbol;
+        // let outputPath = stocksFolder + symbol + '.json';
+        // if (!fs.existsSync(outputPath)) {
+            stockSymbols.push(symbol);
+        // }
+
+        if (stockSymbols.length > limit) break;
+    }
+    return stockSymbols;
+}
+
+function wait(time) {
+    return new Promise(resolve => {
+        setTimeout(resolve, time);
+    });
+}
+
+exports.fetchYahooProfile = function fetchYahooProfile(cb)
+{
+    let stocksFolder = '../convert/yahoo/'
+    var stockCounter = 0;
+    var allSymbols = getStockSymbols(4000);
+    var stockSymbols = [];
+    var lastSymbol = 0;
+    for (let i = 0; i < allSymbols.length; i++) {
+        var symbol = allSymbols[i];
+        var outputPath = '../convert/yahoo/' + symbol + '.json';
+        if (!fs.existsSync(outputPath)) {
+            stockSymbols.push(symbol);
+            lastSymbol = i;
+        }
+        if (stockSymbols.length > 15) break;
+    }
+
+    console.log('allSymbols=' + allSymbols.length);
+    console.log('stockSymbols=' + stockSymbols.length);
+    function parseStock(symbol, data, url) {
+        var stock = {};
+        stockCounter++;
+        if (data !== null)
+        {
+            //console.log(url);
+            // console.log(symbol);
+            const regex = /Sector.{34}(.*)\<\/span\>\<\/p\>\<\/div\><\/div\>/g;
+            let matches = data.match(regex);
+            if (matches === null || matches === undefined) {
+                console.log('not found ' + url);
+                return;
+            }
+            let found = data.match(regex)[0];
+            found = found.split('</span>').join('');
+            found = found.split('<span ').join('');
+            found = found.split('<span>').join('');
+            found = found.split('class="Fw(600)">').join('');
+            found = found.split('</p></div></div>').join('');
+            found = found.split('Full Time ').join('');
+            found = found.split("'").join('');
+            found = found.split('(s)').join('').replace(',','').replace('&amp;','&');
+            // console.log(found);
+            let columns = found.split('<br/>');
+            // console.log(columns);
+            for (const col of columns) {
+                let info = col.split(':');
+                var name = info[0];
+                var value = info[1];
+                // console.log(info);
+                stock.Symbol = symbol;
+                if (value === undefined || value === ' ')
+                    stock[name] = '0';
+                else
+                    stock[name] = value.trim();
+            }
+            // console.log(stock);
+            if (stock.Sector !== undefined) {
+                let outputPath = stocksFolder + symbol + '.json';
+                saveJSON(outputPath, [stock],  "compact");
+            }
+        }
+        // console.log(symbol + '  ' + data.MarketCapitalization);
+        if (stockCounter >= stockSymbols.length) {
+            // let jsonPath = CodeGenLib + "/StockMarketTMP/XPLAT.json";
+            // saveJSON('../convert/yahoo/sectors.json', stockSectors,  "compact");
+            console.log('lastSymbol=' + lastSymbol);
+            cb();
+        }
+    }
+
+    // console.log(stockSymbols.join(', '));
+
+    for (const symbol of stockSymbols) {
+        // let url = 'https://finance.yahoo.com/quote/ELV/profile?p=ELV'
+        let url = 'https://finance.yahoo.com/quote/'+symbol+'/profile?p='+symbol
+
+        console.log('fetching ' + url)
+        request.get({
+            url: url, json: true, headers: {'User-Agent': 'request'}
+        }, (err, res, data) => {
+            if (err) {
+                console.log('Error:', err);
+                parseStock(symbol, null, url);
+            } else if (res.statusCode !== 200) {
+                console.log('Error:' + res.statusCode + ' ' + url);
+                parseStock(symbol, null, url);
+            } else {
+                // data is successfully parsed as a JSON object:
+                // console.log(data);
+                parseStock(symbol, data, url);
+            }
+        });
+
+        // setTimeout(500);
+    }
+}
+
+// 3m OHLC data range
+// http://api.marketstack.com/v1/eod?access_key=b4d9ad0f817fe057764a3eb1ba1f8ff0&symbols=AAPL
+// http://api.marketstack.com/v1/tickers/aapl/eod?access_key=b4d9ad0f817fe057764a3eb1ba1f8ff0
+
+// icons https://www.tradingview.com/markets/stocks-usa/sectorandindustry-sector/commercial-services/
+// https://www.alphavantage.co/documentation/
+exports.fetchStock = function fetchStock(cb, symbol)
+{
+    let stocksFolder = '../convert/stocks/'
+    let sp500 = utils.readCSV('../convert/sp500.csv');
+
+    var stockSymbols = [];
+    for (let i = 0; i < sp500.data.length; i++) {
+        let company = sp500.data[i];
+        let symbol = company.Symbol;
+        let outputPath = stocksFolder + symbol + '.json';
+
+        if (!fs.existsSync(outputPath)) {
+            stockSymbols.push(symbol);
+        }
+
+        if (stockSymbols.length >= 5) break;
+    }
+
+    var columns = ['Symbol', 'Name', 'Country', 'Exchange','Sector', 'Industry', 'MarketCapitalization',
+     'EBITDA', 'PERatio', 'TrailingPE', 'ForwardPE', 'DividendPerShare', 'DividendYield',  'EPS', 'ProfitMargin', 'OperatingMarginTTM',
+    'RevenuePerShareTTM',
+    'RevenueTTM', 'GrossProfitTTM', 'PriceToSalesRatioTTM', 'PriceToBookRatio',
+    'Beta', '52WeekHigh', '52WeekLow', '50DayMovingAverage', '200DayMovingAverage', 'SharesOutstanding'];
+
+    var stockData = [];
+    var stockCounter = 0;
+
+    function parseStock(outputPath, data) {
+        var stock = {};
+        for (const column of columns) {
+            // if (data.MarketCapitalization) {
+                var name = column.replace('TTM','').replace('PERatio','CurrentPE')
+                .replace('Capitalization','Cap').replace('Ratio','').replace('MovingAverage','Average');
+                // stock[name] = data.column;
+                stock[name] = data[column];
+            // }
+        }
+        // stockData.push(stock);
+        if (stock.Sector !== undefined) {
+            saveJSON(outputPath, [stock], "compact");
+        }
+
+        // console.log(symbol + '  ' + data.MarketCapitalization);
+        if (stockCounter >= stockSymbols.length) {
+            // let jsonPath = CodeGenLib + "/StockMarketTMP/XPLAT.json";
+            // saveJSON(outputPath, stockData,  "compact");
+            cb();
+        }
+    }
+
+    for (const symbol of stockSymbols) {
+        // console.log('fetching ' + symbol)
+        let outputPath = stocksFolder + symbol + '.json';
+
+        if (fs.existsSync(outputPath)) {
+            console.log('exits ' + outputPath);
+            stockCounter++;
+            continue;
+        }
+        // var url = 'https://www.alphavantage.co/query?function=OVERVIEW&symbol=' + symbol + '&apikey=demo';
+        // var url = 'https://www.alphavantage.co/query?function=OVERVIEW&symbol=' + symbol + '&apikey=AZ6V4V9MYY61IDL5';
+        // var url = 'https://www.alphavantage.co/query?function=OVERVIEW&symbol=' + symbol + '&apikey=MF509CVMN4P2ACMP';
+        var url = 'https://www.alphavantage.co/query?function=OVERVIEW&symbol=' + symbol + '&apikey=KLP643CXASZEMGZY';
+        // var url = 'https://www.alphavantage.co/query?function=OVERVIEW&symbol=' + symbol + '&apikey=B0KRIHH8G3ODPKGZ';
+        // var url = 'https://www.alphavantage.co/query?function=OVERVIEW&symbol=IMB&apikey=demo';
+        // var url = 'https://www.alphavantage.co/query?function=OVERVIEW&symbol=IMB&apikey=AZ6V4V9MYY61IDL5';
+
+        console.log('fetching ' + url)
+        request.get({
+            url: url, json: true, headers: {'User-Agent': 'request'}
+        }, (err, res, data) => {
+            if (err) {
+                console.log('Error:', err);
+            } else if (res.statusCode !== 200) {
+                console.log('Status:', res.statusCode);
+            } else {
+                // data is successfully parsed as a JSON object:
+                // console.log(data);
+                // console.log('fetching ' + symbol + ' done ')
+            }
+            stockCounter++;
+            parseStock(outputPath, data);
+            // stockCounter = stockCounter + 1;
+        });
+    }
+}
+
+exports.combineYahooProfile = function combineYahooProfile(cb)
+{
+    var fileCounter = 0;
+    var jsonCounter = 0;
+    var jsonStocks = [];
+    var stockSectors = [];
+    var stockIndustry = [];
+    var stockIncomplete = [];
+
+    gulp.src([
+        '../convert/yahoo/*.json',
+    ])
+    // .pipe(es.map(verifySampleFile ))
+    .pipe(es.map(function(file, fileCallback) {
+        fileCounter++;
+        let content = file.contents.toString();
+
+        // console.log('load ' + file.dirname + '/' + file.basename)
+        let jsonArray = JSON.parse(content);
+        jsonCounter += jsonArray.length;
+        // var filePath = getPath(file);
+        for (let item of jsonArray) {
+
+            item.Employees = item.Employees === '' ? 100 : utils.strToNumber(item.Employees);
+            jsonStocks.push(item);
+
+            if (!stockSectors.includes(item.Sector)){
+                 stockSectors.push(item.Sector);
+            }
+            if (!stockIndustry.includes(item.Industry)){
+                 stockIndustry.push(item.Industry);
+            }
+
+            if (item.Sector === undefined || item.Sector === '' ||
+                item.Industry === undefined || item.Industry === '' ||
+                item.Employees === undefined) {
+                stockIncomplete.push(item.Symbol);
+            }
+        //     if (content.indexOf(item.forbidden) >= 0) {
+        //         if (item.valid)
+        //             issues.push(filePath + " - File should use '" + item.valid + "' instead of '" + item.forbidden + "'");
+        //         else
+        //             issues.push(filePath + " - File should not contain '" + item.forbidden + "'");
+        //     }
+        }
+        // if (issues.length > 0) {
+            //console.log("Found issues in " + getPath(file) + " file:");
+            // console.log(issues.join('\n'));
+        // }
+        fileCallback(null, file);
+    }))
+    .on("end", function() {
+        console.log('fileCounter=' + fileCounter)
+        console.log('jsonCounter=' + jsonCounter)
+        // console.log('stockSectors=')
+        // console.log(stockSectors)
+
+        // console.log('stockIndustry=')
+        // console.log(stockIndustry)
+        // console.log('stockIncomplete=')
+        // console.log(stockIncomplete)
+
+        saveJSON("../convert/market-sectors-yahoo.json", jsonStocks,  "compact");
+
+        cb();
+     });
+}
+
+exports.toCSV = function toCSV(cb) {
+    let jsonPath = "../convert/market-sectors-yahoo.json";
+    let jsonFile = fs.readFileSync(jsonPath, "utf8");
+    let items = JSON.parse(jsonFile);
+
+    var lines = [];
+
+    for (let i = 0; i < items.length; i++) {
+        const item = items[i];
+
+        var columns = Object.keys(item);
+        if (i === 0) {
+            var line = '"' + columns.join('","') + '"';
+            lines.push(line);
+        }
+
+        var line = JSON.stringify(item, null, '');
+        for (const column of columns) {
+            line = line.replace('"' + column + '":', '').replace('{','').replace('}','');
+        }
+        lines.push(line);
+    }
+
+    var cvsContent = lines.join('\n');
+    var cvsPath = jsonPath.replace('.json', '.csv');
+    saveFile(cvsPath, cvsContent);
     cb();
 }
