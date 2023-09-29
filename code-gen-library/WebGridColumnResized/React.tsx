515622307dd58904d49e4a74b60d55f12f6f67fa
Martin Dragnev <mddragnev@gmail.com>
2023-09-29 16:34:41 +0300

    Update the handler

--- code-gen-library/WebGridColumnResized/React.tsx
+++ code-gen-library/WebGridColumnResized/React.tsx
@@ -1,10 +1,10 @@
 //begin imports
-import { IgrRowSelectionEventArgs } from 'igniteui-react-grids';
+import { IgrGridBaseDirective, IgrRowSelectionEventArgs } from 'igniteui-react-grids';
 //end imports
 
 export class WebGridColumnResized {
     //begin eventHandler
-    public webGridColumnResized(args: IgrRowSelectionEventArgs): void {
+    public webGridColumnResized(grid: IgrGridBaseDirective, args: IgrRowSelectionEventArgs): void {
         var col = args.detail.column;
         var pWidth = args.detail.prevWidth;
         var nWidth = args.detail.newWidth;
