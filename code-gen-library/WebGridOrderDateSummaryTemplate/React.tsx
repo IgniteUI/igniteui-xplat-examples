a1948e16c2b4b3c20547fa06fd867b03cbeae72c
gmurray81 <gmurray@gmail.com>
2023-10-03 16:49:11 -0400

    add missing template

---
+++ code-gen-library/WebGridOrderDateSummaryTemplate/React.tsx
@@ -0,0 +1,17 @@
+//begin imports
+import { IgrCellTemplateContext } from 'igniteui-react-grids';
+//end imports
+
+import { CodeGenHelper } from 'igniteui-webcomponents-core';
+
+export class WebGridOrderDateSummaryTemplate {
+//begin template
+//begin content
+    public webGridOrderDateSummaryTemplate = (props: {dataContext: IgrCellTemplateContext}) => {
+        console.log("TODO");
+        //TODO
+        return (<></>);
+    }
+//end content
+//end template
+}
