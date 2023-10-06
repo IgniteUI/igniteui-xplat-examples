176b487d6df80f054d0ac3360dd653236463ee07
Maya Kirova <MKirova@infragistics.com>
2023-10-06 16:17:14 +0300

    Add event args type and other minor fixes.

--- code-gen-library/WebGridPinFirstGroupToggle/Web.ts
+++ code-gen-library/WebGridPinFirstGroupToggle/Web.ts
@@ -1,14 +1,15 @@
 //begin imports
 import { IgcGridComponent } from 'igniteui-webcomponents-grids/grids';
+import { IgcPropertyEditorPropertyDescriptionButtonClickEventArgs } from 'igniteui-webcomponents-layouts';
 //end imports
 
 import { CodeGenHelper } from 'igniteui-webcomponents-core';
 
 export class WebGridPinFirstGroupToggle {
     //begin eventHandler
-    public webGridPinFirstGroupToggle(args: any): void {
+    public webGridPinFirstGroupToggle(sender: any, args: IgcPropertyEditorPropertyDescriptionButtonClickEventArgs): void {
         const grid: IgcGridComponent = CodeGenHelper.getDescription<IgcGridComponent>("content")
-        const firstColumnGroup = grid.columns.filter(c => c.header === 'General Information')[0];
+        const firstColumnGroup = grid.visibleColumns.filter((c: any) => c.header === 'General Information')[0];
         firstColumnGroup.pinned = !firstColumnGroup.pinned;
         grid.markForCheck();
     }
