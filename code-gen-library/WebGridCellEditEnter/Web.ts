848f65edc932490c227016bcfd27fdf5e87ed8d0
IMinchev64 <iminchev@infragistics.com>
2023-10-06 18:11:32 +0300

    [React] Add WebGridCellEditEnter event args type

--- code-gen-library/WebGridCellEditEnter/Web.ts
+++ code-gen-library/WebGridCellEditEnter/Web.ts
@@ -1,12 +1,12 @@
 //begin imports
-import { IgcGridComponent } from 'igniteui-webcomponents-grids/grids';
+import { IgcGridComponent, IgcGridEditEventArgs } from 'igniteui-webcomponents-grids/grids';
 //end imports
 
 import { CodeGenHelper } from 'igniteui-webcomponents-core';
 
 export class WebGridCellEditEnter {
     //begin eventHandler
-    public webGridCellEditEnter(args: any): void {
+    public webGridCellEditEnter(args: CustomEvent<IgcGridEditEventArgs>): void {
         let container = document.getElementById("container");
         const message = document.createElement("p");
         message.textContent = `=> 'cellEditEnter' with 'value':` + args.detail.oldValue, args.detail.cancel;
