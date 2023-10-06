b2ea3897a06ec17dc69600fcf77f29c5266b18a5
IMinchev <61944284+IMinchev64@users.noreply.github.com>
2023-10-06 18:13:45 +0300

    Update Web.ts

--- code-gen-library/WebGridCellEditExit/Web.ts
+++ code-gen-library/WebGridCellEditExit/Web.ts
@@ -1,12 +1,12 @@
 //begin imports
-import { IgcGridComponent, IgrGridEditDoneEventArgs } from 'igniteui-webcomponents-grids/grids';
+import { IgcGridComponent, IgcGridEditDoneEventArgs } from 'igniteui-webcomponents-grids/grids';
 //end imports
 
 import { CodeGenHelper } from 'igniteui-webcomponents-core';
 
 export class WebGridCellEditExit {
     //begin eventHandler
-    public webGridCellEditExit(args: CustomEvent<IgrGridEditDoneEventArgs>): void {
+    public webGridCellEditExit(args: CustomEvent<IgcGridEditDoneEventArgs>): void {
         let container = document.getElementById("container");
         const message = document.createElement("p");
         message.textContent = `=> 'cellEditExit'`;
