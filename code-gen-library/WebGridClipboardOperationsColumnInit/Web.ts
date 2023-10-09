2636fd98aa2d822ef489a26095e20bd6c4597974
IMinchev64 <iminchev@infragistics.com>
2023-10-09 10:14:40 +0300

    WebGridClipboardOperationsColumnInit event args type

--- code-gen-library/WebGridClipboardOperationsColumnInit/Web.ts
+++ code-gen-library/WebGridClipboardOperationsColumnInit/Web.ts
@@ -1,12 +1,12 @@
 ﻿//begin imports
-import { IgcGridComponent } from 'igniteui-webcomponents-grids/grids';
+import { IgcGridComponent, IgcColumnComponentEventArgs } from 'igniteui-webcomponents-grids/grids';
 //end imports
 
 import { CodeGenHelper } from 'igniteui-webcomponents-core';
 
 export class WebGridClipboardOperationsColumnInit {
     //begin eventHandler
-    public webGridClipboardOperationsColumnInit(args: any): void {
+    public webGridClipboardOperationsColumnInit(args: CustomEvent<IgcColumnComponentEventArgs>): void {
         let column = args.detail;
         column.formatter = (e: any) => { return "** " + e + " **" };
         column.header = "🎉" + column.field;
