17a3d4783a9e012319f361d5771b4fc219b7e48a
Maya Kirova <MKirova@infragistics.com>
2023-10-11 16:28:26 +0300

    Use grid's markForCheck to trigger update.

--- code-gen-library/WebGridNestedDataCellTemplate/React.tsx
+++ code-gen-library/WebGridNestedDataCellTemplate/React.tsx
@@ -3,6 +3,8 @@ import { IgrCellTemplateContext } from 'igniteui-react-grids';
 import { IgrExpansionPanel } from 'igniteui-react';
 //end imports
 
+import { CodeGenHelper } from 'igniteui-webcomponents-core';
+
 export class WebGridNestedDataCellTemplate {
     //begin template
     //begin content
@@ -10,6 +12,7 @@ export class WebGridNestedDataCellTemplate {
         if (props.dataContext.cell.value != null) {
             if (props.dataContext.cell.value.length === 0) return <></>;
             const value = props.dataContext.cell.value[0];
+            var grid = CodeGenHelper.getDescription<IgcGridComponent>("content");
             return (
         <>
             <IgrExpansionPanel>
@@ -21,14 +24,14 @@ export class WebGridNestedDataCellTemplate {
                         <label htmlFor="title" style={{width: "2rem", margin: "0rem"}}>Title</label>
                         <input id='Title' type="text" name="title" value={value.Title} onChange={(e: any) => {
                              props.dataContext.cell.value[0][e.target.id] = e.target.value;
-                             this.forceUpdate();
+                             grid.markForCheck();
                             }} style={{textOverflow: "ellipsis"}} />
                     </div>
                     <div style={{display: "flex", alignItems: "center"}}>
                         <label htmlFor="age" style={{width: "2rem", margin: "0rem"}}>Age</label>
                         <input id='Age' type="number" name="title" value={value.Age} onChange={(e: any) => {
                                 props.dataContext.cell.value[0][e.target.id] = e.target.value;
-                                this.forceUpdate();
+                                grid.markForCheck();
                             }} style={{textOverflow: "ellipsis"}} />
                     </div>
                 </div>
