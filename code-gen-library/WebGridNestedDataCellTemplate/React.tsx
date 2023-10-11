005f386c9faf4486d7ec13202ddcb1670b116ee7
Maya Kirova <MKirova@infragistics.com>
2023-10-11 13:28:55 +0300

    Fix issue with input not updating on change inside template.

--- code-gen-library/WebGridNestedDataCellTemplate/React.tsx
+++ code-gen-library/WebGridNestedDataCellTemplate/React.tsx
@@ -9,20 +9,27 @@ export class WebGridNestedDataCellTemplate {
     public webGridNestedDataCellTemplate = (props: {dataContext: IgrCellTemplateContext}) => {
         if (props.dataContext.cell.value != null) {
             if (props.dataContext.cell.value.length === 0) return <></>;
+            const value = props.dataContext.cell.value[0];
             return (
         <>
             <IgrExpansionPanel>
                 <div slot="title" style={{fontSize: "1.1em", fontWeight: "bold", marginTop: "1rem", marginBottom: "0.25rem"}}>
-                {props.dataContext.cell.value[0].Name}
+                {value.Name}
                 </div>
                 <div className="description">
                     <div style={{display: "flex", alignItems: "center"}}>
                         <label htmlFor="title" style={{width: "2rem", margin: "0rem"}}>Title</label>
-                        <input id='Title' type="text" name="title" value={props.dataContext.cell.value[0].Title} style={{textOverflow: "ellipsis"}} />
+                        <input id='Title' type="text" name="title" value={value.Title} onChange={(e: any) => {
+                             props.dataContext.cell.value[0][e.target.id] = e.target.value;
+                             this.forceUpdate();
+                            }} style={{textOverflow: "ellipsis"}} />
                     </div>
                     <div style={{display: "flex", alignItems: "center"}}>
                         <label htmlFor="age" style={{width: "2rem", margin: "0rem"}}>Age</label>
-                        <input id='Age' type="text" name="title" value={props.dataContext.cell.value[0].Age} style={{textOverflow: "ellipsis"}} />
+                        <input id='Age' type="number" name="title" value={value.Age} onChange={(e: any) => {
+                                props.dataContext.cell.value[0][e.target.id] = e.target.value;
+                                this.forceUpdate();
+                            }} style={{textOverflow: "ellipsis"}} />
                     </div>
                 </div>
             </IgrExpansionPanel>
