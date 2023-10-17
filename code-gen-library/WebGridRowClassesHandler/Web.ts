5a0dcf00e5f5cd0df272bfa404b50709770f7aa3
Martin Dragnev <mddragnev@gmail.com>
2023-10-17 13:41:38 +0300

    Add rowclasses sample for all platforms

---
+++ code-gen-library/WebGridRowClassesHandler/Web.ts
@@ -0,0 +1,20 @@
+//begin imports
+import { IgcRowType } from "igniteui-webcomponents-grids/grids";
+//end imports
+
+export class WebGridRowClassesHandler {
+  //begin eventHandler
+  public webGridRowClassesHandler = {
+    activeRow: (row: IgcRowType) => row.index % 2,
+  };
+  //end eventHandler
+
+  public requiredStyles = `
+    <!--begin styles-->
+    .activeRow {
+        border-top: 2px solid #fc81b8;
+        border-left: 3px solid #e41c77;
+    }
+    <!--end styles-->
+    `;
+}
