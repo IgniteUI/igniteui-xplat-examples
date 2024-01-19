db42eb807aab28fe566832201b2aac1dee5db604
gmurray81 <gmurray@gmail.com>
2024-01-19 13:31:43 -0500

    fix fetching of props

--- code-gen-library/PropertyEditorInitAggregationsOnViewInit/Web.ts
+++ code-gen-library/PropertyEditorInitAggregationsOnViewInit/Web.ts
@@ -1,5 +1,5 @@
 //begin imports
-import { IgcToolbarComponent, IgcToolActionLabelComponent, IgcToolActionComponent, IgcToolCommandEventArgs } from 'igniteui-webcomponents-layouts';
+import { IgcPropertyEditorPanelComponent } from 'igniteui-webcomponents-layouts';
 //end imports
 
 
@@ -9,9 +9,9 @@ export class PropertyEditorInitAggregationsOnViewInit {
     public propertyEditorInitAggregationsOnViewInit(): void {
 	
         var editor = CodeGenHelper.getDescription<IgcPropertyEditorPanelComponent>("editor");
-        var initialSummaries = editor.properties.toArray().filter((p) => p.label == "Initial Summaries")[0];
+        var initialSummaries = editor.actualProperties.filter((p) => p.label == "Initial Summaries")[0];
         initialSummaries.dropDownNames = ["Sum(Sales) as Sales", "Avg(Sales) as Sales", "Min(Sales) as Sales", "Max(Sales) as Sales", "Count(Sales) as Sales" ];
-        initialSummaries.dropDownValues = ["Sum(Sales) as Sales", "Avg(Sales) as Sales", "Min(Sales) as Sales", "Max(Sales) as Sales", "Count(Sales) as Sales"];
+        initialSummaries.dropDownValues = ["Sum(Sales) as Sales", "Avg(Sales) as Sales", "Min(Sales) as Sales", "Max(Sales) as Sales", "Count(Sales) as Sales" ];
 		
     }
     //end eventHandler
