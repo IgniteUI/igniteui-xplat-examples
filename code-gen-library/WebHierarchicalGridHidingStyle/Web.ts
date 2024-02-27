1fcdb5eaf7362f71695ff55c09c12dc70b9014f6
Georgi Anastasov <ggeorgianastasov@gmail.com>
2024-02-27 17:34:36 +0200

    Add hgrid hiding samples

---
+++ code-gen-library/WebHierarchicalGridHidingStyle/Web.ts
@@ -0,0 +1,36 @@
+//begin imports
+//end imports
+
+export class WebHierarchicalGridHidingStyle {
+    //begin eventHandler
+    public WebHierarchicalGridHidingStyle(): void {
+        //OMIT HANDLER
+    }
+    //end eventHandler
+    public requiredStyles = `
+<!--begin styles-->
+#hierarchicalGrid igc-grid-toolbar-actions, igx-column-actions {
+    --ig-column-actions-background-color: #292826;
+    --ig-column-actions-title-color: #ffcd0f;
+    --ig-checkbox-tick-color: #292826;
+    --ig-checkbox-label-color: #ffcd0f;
+    --ig-checkbox-empty-color: #ffcd0f;
+    --ig-checkbox-fill-color: #ffcd0f;
+    --ig-input-group-idle-text-color: white;
+    --ig-input-group-filled-text-color: #ffcd0f;
+    --ig-input-group-focused-text-color: #ffcd0f;
+    --ig-input-group-focused-border-color: #ffcd0f;
+    --ig-input-group-focused-secondary-color: #ffcd0f;
+    --ig-button-foreground: #292826;
+    --ig-button-background: #ffcd0f;
+    --ig-button-hover-background: #404040;
+    --ig-button-hover-foreground: #ffcd0f;
+    --ig-button-focus-background: #ffcd0f;
+    --ig-button-focus-foreground: black;
+    --ig-button-focus-visible-background: #ffcd0f;
+    --ig-button-focus-visible-foreground: black;
+    --ig-button-disabled-foreground: #ffcd0f;
+}
+<!--end styles-->
+    `;
+}
