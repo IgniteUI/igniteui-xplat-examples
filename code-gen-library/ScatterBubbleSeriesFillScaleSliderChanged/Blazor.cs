6128fb5710b94eb8eb9e0c566f9cdb5d7f876bbe
agoldenbaum <agoldenbaum@infragistics.com>
2024-03-07 16:23:27 -0500

    Add bubble series fill scale sample

---
+++ code-gen-library/ScatterBubbleSeriesFillScaleSliderChanged/Blazor.cs
@@ -0,0 +1,20 @@
+//begin imports
+using IgniteUI.Blazor.Controls;
+using System;
+using System.Linq;
+//end imports
+
+
+
+public class ScatterBubbleSeriesFillScaleSliderChanged
+{
+
+    //begin eventHandler
+    public void ScatterBubbleSeriesFillScaleSliderChanged(IgbPropertyEditorPropertyDescriptionChangedEventArgs args)
+    {
+        //var item = CodeGenHelper.GetDescription<IgbPropertyEditorPanel>("editor").Properties.Where((p) => p.PropertyPath == "MarkerType").First();
+        //var value = (string)item.PrimitiveValue;
+        //var chart = CodeGenHelper.GetDescription<IgbCategoryChart>("content");
+    }
+    //end eventHandler
+}
