099c4f7bb558a5e91f4450953eb7d2d656251248
Martin Dragnev <mddragnev@gmail.com>
2024-03-12 15:33:58 +0200

    Add cell styles sample

---
+++ code-gen-library/WebHierarchicalGridCellStylesHandler/Web.ts
@@ -0,0 +1,22 @@
+//begin imports
+import { IgcPropertyEditorPropertyDescriptionButtonClickEventArgs } from 'igniteui-webcomponents-layouts';
+import { IgcGridComponent, IgcRowType } from 'igniteui-webcomponents-grids/grids';
+//end imports
+
+import { CodeGenHelper } from 'igniteui-webcomponents-core';
+
+export class WebHierarchicalGridCellStylesHandler {
+//begin template
+//begin content
+    public webHierarchicalGridCellStylesHandler = {
+        background: (rowData: any, columnKey: any, cellValue: any, rowIndex: any) => rowIndex % 2 === 0 ? "#EFF4FD" : null,
+        color: (rowData: any, columnKey: any, cellValue: any, rowIndex: any) => {
+            if (columnKey === "HasGrammyAward") {
+                return cellValue ? "#28a745" : "#dc3545";
+            }
+            return undefined;
+        }
+    };
+//end content
+//end template
+}
