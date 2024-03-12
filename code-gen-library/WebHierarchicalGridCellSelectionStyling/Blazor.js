7784ffacf0584f4e0f3be1e875313ba71ce5cf05
Ivan Kitanov <IKitanov@infragistics.com>
2024-03-12 16:23:12 +0200

    Add hgrid cell selection samples

---
+++ code-gen-library/WebHierarchicalGridCellSelectionStyling/Blazor.js
@@ -0,0 +1,15 @@
+//begin eventHandler
+igRegisterScript("WebHierarchicalGridStyling", (args) => {
+    //OMIT HANDLER
+}, false);
+//end eventHandler
+
+let requiredStyles = `
+<!--begin styles-->
+#hGrid {
+    --cell-selected-text-color: #FFFFFF;
+    --cell-active-border-color: #0578c4;
+    --cell-selected-background: #0578c4;
+}
+<!--end styles-->
+`;
