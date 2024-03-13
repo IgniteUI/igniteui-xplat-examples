74f086d0473a7f46667b0cc16b1ed8e1b0349936
mddifilippo89 <mdifilippo@infragistics.com>
2024-03-13 15:48:22 -0400

    mdd-update-event-import
    mdd-update-event-import

--- code-gen-library/WebGridExportEventMultiColumnHeaders/WebComponents.ts
+++ code-gen-library/WebGridExportEventMultiColumnHeaders/WebComponents.ts
@@ -1,12 +1,12 @@
 //begin imports
-import { IgcGridComponent, IgcExporterEvent } from 'igniteui-webcomponents-grids/grids';
+import { IgcGridComponent, IgcExporterEventArgs } from 'igniteui-webcomponents-grids/grids';
 //end imports
 
 import { CodeGenHelper } from 'igniteui-webcomponents-core';
 
 export class WebGridExportEventMultiColumnHeaders {
     //begin eventHandler
-    public webGridExportEventMultiColumnHeaders(args: CustomEvent<IgcExporterEvent>): void {
+    public webGridExportEventMultiColumnHeaders(args: CustomEvent<IgcExporterEventArgs>): void {
         if (args.detail.options) {
             args.detail.options.ignoreMultiColumnHeaders = false;
         }
