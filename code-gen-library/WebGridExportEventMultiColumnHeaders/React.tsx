26ea16fdc09f5a54d0f35b5781a9f1a4b8dc2801
Maya Kirova <MKirova@infragistics.com>
2024-03-14 18:30:41 +0200

    Update sample to use correct type name.

--- code-gen-library/WebGridExportEventMultiColumnHeaders/React.tsx
+++ code-gen-library/WebGridExportEventMultiColumnHeaders/React.tsx
@@ -1,12 +1,12 @@
 //begin imports
-import { IgrExporterEventEventArgs } from 'igniteui-react-grids';
+import { IgrExporterEventArgs } from 'igniteui-react-grids';
 //end imports
 
 import { CodeGenHelper } from 'igniteui-webcomponents-core';
 
 export class WebGridExportEventMultiColumnHeaders {
     //begin eventHandler
-    public webGridExportEventMultiColumnHeaders(args: CustomEvent<IgrExporterEventEventArgs>): void {
+    public webGridExportEventMultiColumnHeaders(args: CustomEvent<IgrExporterEventArgs>): void {
         if (args.detail.options) {
             args.detail.options.ignoreMultiColumnHeaders = false;
         }
