9670af3efc26f2b22131876f5050976dda956e70
Georgi Anastasov <ggeorgianastasov@gmail.com>
2024-03-20 16:03:30 +0200

    Add hrid row editing samples

---
+++ code-gen-library/WebHierarchicalGridRowEditingStyle/Blazor.js
@@ -0,0 +1,14 @@
+//begin eventHandler
+igRegisterScript("WebHierarchicalGridRowEditingStyle", (args) => {
+    //OMIT HANDLER
+}, false);
+//end eventHandler
+
+let requiredStyles = `
+<!--begin styles-->
+#hierarchicalGrid {
+    --ig-banner-banner-background: #e3e3e3;
+    --ig-banner-banner-message-color: #423589;
+}
+<!--end styles-->
+`;
