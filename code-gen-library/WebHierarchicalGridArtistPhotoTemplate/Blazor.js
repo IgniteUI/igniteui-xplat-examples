bcadf04d2bb4cae744963e795cd72dcf097058fa
ttonev <ttonev@infragistics.com>
2024-04-09 10:28:21 +0300

    removed unnecesary templates

--- code-gen-library/WebHierarchicalGridArtistPhotoTemplate/Blazor.js
+++
@@ -1,8 +0,0 @@
-//begin template
-igRegisterScript("WebHierarchicalGridArtistPhotoTemplate", (ctx) => {
-
-    if (ctx.cell.value) {
-        return html`<img src="${ctx.cell.value}"/>`;
-    }
-});
-//end template
