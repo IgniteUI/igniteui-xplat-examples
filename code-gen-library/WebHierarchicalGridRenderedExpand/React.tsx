291a11b2ada79d1fb3b3330f8d15cfb0ed1e1f2f
Georgi Anastasov <ggeorgianastasov@gmail.com>
2024-04-10 18:43:37 +0300

    Resolve React formatter error

--- code-gen-library/WebHierarchicalGridRenderedExpand/React.tsx
+++ code-gen-library/WebHierarchicalGridRenderedExpand/React.tsx
@@ -7,8 +7,10 @@ import { CodeGenHelper } from 'igniteui-webcomponents-core';
 export class WebHierarchicalGridRenderedExpand {
     //begin eventHandler
     public webHierarchicalGridRenderedExpand(args:any): void {
-        this.hierarchicalGrid.getColumnByName("Debut").formatter = (value: number) => Math.floor(value / 10) * 10 + 's';
         this.hierarchicalGrid.expandAll();
+        setTimeout(() => {
+            this.hierarchicalGrid.getColumnByName("Debut").formatter = (value: number) => Math.floor(value / 10) * 10 + 's';
+        }, 100);
     }
     //end eventHandler
 }
