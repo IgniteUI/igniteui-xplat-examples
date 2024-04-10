0764a8abcd8c71f903085b34b9c7948efb4d5b9b
Deyan Kamburov <dkamburov@infragistics.com>
2024-04-10 13:32:46 +0300

    chore(hgrid): rename IgcExporterEvent to IgcExporterEventArgs

--- code-gen-library/WebHierarchicalGridExportMultiColumnHeaders/Web.ts
+++ code-gen-library/WebHierarchicalGridExportMultiColumnHeaders/Web.ts
@@ -1,12 +1,12 @@
 //begin imports
-import { IgcHierarchicalGridComponent, IgcExporterEvent } from 'igniteui-webcomponents-grids/grids';
+import { IgcHierarchicalGridComponent, IgcExporterEventArgs } from 'igniteui-webcomponents-grids/grids';
 //end imports
 
 import { CodeGenHelper } from 'igniteui-webcomponents-core';
 
 export class WebHierarchicalGridExportMultiColumnHeaders {
     //begin eventHandler
-    public webHierarchicalGridExportMultiColumnHeaders(args: CustomEvent<IgcExporterEvent>): void {
+    public webHierarchicalGridExportMultiColumnHeaders(args: CustomEvent<IgcExporterEventArgs>): void {
         if (args.detail.options) {
             args.detail.options.ignoreMultiColumnHeaders = false;
         }
