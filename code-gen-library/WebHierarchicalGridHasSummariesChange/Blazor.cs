04045aa35b08a0b2d80d19724d74829bc9770a6a
Georgi Anastasov <ggeorgianastasov@gmail.com>
2024-04-10 16:07:31 +0300

    Correct column order

--- code-gen-library/WebHierarchicalGridHasSummariesChange/Blazor.cs
+++ code-gen-library/WebHierarchicalGridHasSummariesChange/Blazor.cs
@@ -12,11 +12,11 @@ public class WebHierarchicalGridHasSummariesChange
 
         var column1 = this.hierarchicalGrid.ActualColumns[1];
         var column2 = this.hierarchicalGrid.ActualColumns[3];
-        var column2 = this.hierarchicalGrid.ActualColumns[4];
+        var column3 = this.hierarchicalGrid.ActualColumns[4];
 
         column1.HasSummary = newValue;
         column2.HasSummary = newValue;
-        column2.HasSummary = newValue;
+        column3.HasSummary = newValue;
     }
     //end eventHandler
 }
