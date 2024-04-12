ce234fd339266a29e1dbefab96efd0b3d903f893
Georgi Anastasov <48180072+georgianastasov@users.noreply.github.com>
2024-04-12 14:24:58 +0300

    Add hgrid row editing samples (#531)
    * Add hrid row editing samples
    
    * Correct definition for WebHierarchicalGridRowEditingStyle

---
+++ code-gen-library/WebHierarchicalGridRowEditingStyle/Web.ts
@@ -0,0 +1,18 @@
+//begin imports
+//end imports
+
+export class WebHierarchicalGridRowEditingStyle {
+    //begin eventHandler
+    public webHierarchicalGridRowEditingStyle(): void {
+        //OMIT HANDLER
+    }
+    //end eventHandler
+    public requiredStyles = `
+<!--begin styles-->
+#hierarchicalGrid {
+    --ig-banner-banner-background: #e3e3e3;
+    --ig-banner-banner-message-color: #423589;
+}
+<!--end styles-->
+    `;
+}
