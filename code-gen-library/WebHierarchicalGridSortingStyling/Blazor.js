665ba88ffa178b385cd9717651ecdec2b68d446e
Maya Kirova <MKirova@infragistics.com>
2024-04-15 10:53:30 +0300

    chore(*): Fix merge conflict.

---
+++ code-gen-library/WebHierarchicalGridSortingStyling/Blazor.js
@@ -0,0 +1,14 @@
+//begin eventHandler
+igRegisterScript("WebHierarchicalGridSortingStyling", (args) => {
+    //OMIT HANDLER
+}, false);
+//end eventHandler
+
+let requiredStyles = `
+<!--begin styles-->
+#hierarchicalGrid {
+    --ig-grid-sorted-header-icon-color: #ffb06a;
+    --ig-grid-sortable-header-icon-hover-color: black;
+}
+<!--end styles-->
+`;
