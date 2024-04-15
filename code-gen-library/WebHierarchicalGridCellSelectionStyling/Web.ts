275d9b8ac2ffc00c2ef3e9af4a2b6e683ccb1dfe
Ivan Kitanov <IKitanov@infragistics.com>
2024-04-15 18:24:45 +0300

    Updating css properties

--- code-gen-library/WebHierarchicalGridCellSelectionStyling/Web.ts
+++ code-gen-library/WebHierarchicalGridCellSelectionStyling/Web.ts
@@ -10,9 +10,9 @@ export class WebHierarchicalGridCellSelectionStyling {
     public requiredStyles = `
 <!--begin styles-->
 #hGrid {
-    --cell-selected-text-color: #FFFFFF;
-    --cell-active-border-color: #0578c4;
-    --cell-selected-background: #0578c4;
+    --ig-grid--cell-selected-text-color: #FFFFFF;
+    --ig-grid--cell-active-border-color: #0578c4;
+    --ig-grid--cell-selected-background: #0578c4;
 }
 <!--end styles-->
     `;
