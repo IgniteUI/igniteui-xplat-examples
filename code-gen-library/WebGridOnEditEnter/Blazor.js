49b28313dcc85b1837e2f6861afeee5f6c30cc0e
Mariela Tihova <MTihova@infragistics.com>
2025-03-18 18:10:43 +0200

    Fix for WC and event for Blazor

---
+++ code-gen-library/WebGridOnEditEnter/Blazor.js
@@ -0,0 +1,14 @@
+//begin template
+igRegisterScript("WebGridOnEditEnter", (args) => {
+    const column = args.detail.column;
+    if(column.field === 'ReorderLevel') {
+        setTimeout(() => {
+            const rowId = args.detail.cellID.rowID;
+            const columnId = args.detail.cellID.columnID;
+            const inputTemplateId = `edit-cell-${rowId}-${columnId}`;
+            const x = document.getElementById(inputTemplateId);
+            x?.focus();
+        });
+    }
+}, false);
+//end template
--- code-gen-library/WebGridOnEditEnter/WebComponents.ts
+++ code-gen-library/WebGridOnEditEnter/WebComponents.ts
@@ -5,12 +5,12 @@ import { IgcGridBaseDirective, IgcGridEditEventArgs } from 'igniteui-webcomponen
 
 export class WebGridOnEditEnter {
     //begin eventHandler
-    public webGridOnEditEnter(s: IgcGridBaseDirective, e: IgcGridEditEventArgs): void {
-        const column = s.getColumnByVisibleIndex(e.detail.cellID.columnID);
-        if(column.field === 'ReorderLevel') {      
+    public webGridOnEditEnter(args: IgcGridEditEventArgs): void {
+        const column = args.detail.column;
+        if(column.field === 'ReorderLevel') {
             setTimeout(() => {
-                const rowId = e.detail.cellID.rowID;
-                const columnId = e.detail.cellID.columnID;
+                const rowId = args.detail.cellID.rowID;
+                const columnId = args.detail.cellID.columnID;
                 const inputTemplateId = `edit-cell-${rowId}-${columnId}`;
                 const x = document.getElementById(inputTemplateId);
                 x?.focus();
