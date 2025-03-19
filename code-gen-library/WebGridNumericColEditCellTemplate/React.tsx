a667aa99d5a2b498bc27db787fda391a75a6de98
Mariela Tihova <MTihova@infragistics.com>
2025-03-19 13:41:28 +0200

    Adding onEditEnter event for React

--- code-gen-library/WebGridNumericColEditCellTemplate/React.tsx
+++ code-gen-library/WebGridNumericColEditCellTemplate/React.tsx
@@ -9,10 +9,14 @@ export class WebGridNumericColEditCellTemplate {
     public webGridNumericColEditCellTemplate = (e: { dataContext: IgrCellTemplateContext }) => {
 
         const cell = e.dataContext.cell;
+        const rowId = cell.id.rowID;
+        const columnId = cell.id.columnID;
+        const inputTemplateId = `edit-cell-${rowId}-${columnId}`;
 
         return (
             <IgrInput 
-                type="number" 
+                type="number"
+                id={inputTemplateId} 
                 name={cell.id.rowID} 
                 value={cell.editValue} 
                 inputOcurred={(s:any, e: any) => {
---
+++ code-gen-library/WebGridOnEditEnter/React.tsx
@@ -0,0 +1,22 @@
+//begin imports
+import { IgrGridBaseDirective, IgrGridEditEventArgs } from 'igniteui-react-grids';
+
+//end imports
+
+export class WebGridOnEditEnter {
+    //begin eventHandler
+    public webGridOnEditEnter(s: IgrGridBaseDirective, e: IgrGridEditEventArgs): void {
+
+        const column = s.getColumnByVisibleIndex(e.detail.cellID.columnID);
+        if(column.field === 'ReorderLevel') {
+            setTimeout(() => {
+                const rowId = e.detail.cellID.rowID;
+                const columnId = e.detail.cellID.columnID;
+                const inputTemplateId = `edit-cell-${rowId}-${columnId}`;
+                const x = document.getElementById(inputTemplateId);
+                x?.focus();
+            });
+        }
+    }
+    //end eventHandler
+}
