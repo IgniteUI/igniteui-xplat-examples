28b02d737504776bf84fdcc4abf084f127878ba9
IGvaleries <vsuszynski@infragistics.com>
2025-04-25 18:13:47 -0400

    more test additions

---
+++ code-gen-library/TestsSetValueOverlayAxisAnnotationToTEST/WPF.cs
@@ -0,0 +1,14 @@
+//begin imports
+using Infragistics.Controls.Charts;
+//end imports
+
+public class TestsSetValueOverlayAxisAnnotationToTEST
+{
+    //begin eventHandler
+    //WPF: Infragistics.Controls.Charts.AxisFormatLabelEventHandler
+    public string TestsSetValueOverlayAxisAnnotationToTEST(object sender, object item)
+    {
+		return "TEST";
+    }
+    //end eventHandler
+}
