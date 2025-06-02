7fdf17bb1bc71c195ab19733e49ae30b332f7853
mddifilippo89 <mdifilippo@infragistics.com>
2025-06-02 13:56:09 -0400

    Update React.tsx

--- code-gen-library/WebGridRendered/React.tsx
+++ code-gen-library/WebGridRendered/React.tsx
@@ -1,6 +1,8 @@
 //begin imports
-import { IgrRowSelectionEventArgs, IgrComponentBoolValueChangedEventArgs } from 'igniteui-react-grids';
+import { IgrRowSelectionEventArgs } from 'igniteui-react-grids';
 import { IgrGridComponent } from 'igniteui-react-grids';
+import { IgrComponentBoolValueChangedEventArgs } from 'igniteui-react';
+
 //end imports
 
 import { CodeGenHelper } from 'igniteui-webcomponents-core';
