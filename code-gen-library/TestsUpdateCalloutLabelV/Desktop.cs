c9e02ebc3e4d6b4544799d653db5db517eaf5a19
gmurray81 <gmurray@gmail.com>
2026-02-18 22:36:52 -0500

    Correct variable declaration in TestsUpdateCalloutLabelV
    Fix variable declaration for 'dict' in TestsUpdateCalloutLabelV method.
    Co-authored-by: gmurray81 <4264873+gmurray81@users.noreply.github.com>

--- code-gen-library/TestsUpdateCalloutLabelV/Desktop.cs
+++ code-gen-library/TestsUpdateCalloutLabelV/Desktop.cs
@@ -12,7 +12,7 @@ public class TestsUpdateCalloutLabelV
     {
         var item = args.Item;
         if (item is IDictionary<string, object>) {
-            var dict = (IDictionary<string, object>)item
+            var dict = (IDictionary<string, object>)item;
             var label = dict.ContainsKey("Label") ? dict["Label"].ToString() : "";
             var value = dict.ContainsKey("Value") ? dict["Value"].ToString() : "";
             args.Label = label + "-V-" + value
