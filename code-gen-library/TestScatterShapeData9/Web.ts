de982212a8a94ff78c04a40feb69d7501ce72b4e
IGvaleries <vsuszynski@infragistics.com>
2026-03-06 16:50:35 -0500

    files for polyline / polygon series updating tests
    Co-authored-by: gmurray81 <4264873+gmurray81@users.noreply.github.com>

---
+++ code-gen-library/TestScatterShapeData9/Web.ts
@@ -0,0 +1,28 @@
+//begin imports
+//end imports
+
+//begin data
+export class TestScatterShapeData2 extends Array<object>
+{
+   public constructor(){
+	   const newItems = [{ "Value": 10,"Points":[[
+                     { x: 10, y: 10 },
+                     { x: 10, y: 40 },
+                     { x: 40, y: 40 },
+                     { x: 40, y: 10 }]]},              
+                     {"Value": 20,"Points": [[{ x: 60, y: 10 },
+                     { x: 60, y: 40 },
+                     { x: 90, y: 40 },
+                     { x: 90, y: 10 }]]},                
+                     {"Value": 30,"Points": [[{ x: 10, y: 60 },
+                     { x: 10, y: 90 },
+                     { x: 40, y: 90 },
+                     { x: 40, y: 60 }]]}
+                    ]}];
+                 
+            super(...newItems.slice(0));
+        }
+   
+   }
+
+//end data
