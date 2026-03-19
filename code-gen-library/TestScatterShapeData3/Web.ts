db2d0b3f3f032d330027234510f00821274c089f
IGvaleries <vsuszynski@infragistics.com>
2026-03-19 15:27:06 -0400

    update for andriod/kotlin

--- code-gen-library/TestScatterShapeData3/Web.ts
+++ code-gen-library/TestScatterShapeData3/Web.ts
@@ -5,7 +5,7 @@
 export class TestScatterShapeData3 extends Array<object>
 {
    public constructor(){
-	    const newItems = [{ "Value": 10,"Points":[[
+	    const newItems = [{ "Value": 10,"Points":[
                      {"X":10,"Y":10},
 					 {"X":10,"Y":40},
 					 {"X":40,"Y":40},
@@ -13,8 +13,8 @@ export class TestScatterShapeData3 extends Array<object>
 					 {"X":60,"Y":10},
 					 {"X":60,"Y":40},
 					 {"X":90,"Y":40},
-					 {"X":90,"Y":10}]]},              
-                     {"Value": 20,"Points": [[
+					 {"X":90,"Y":10}]},              
+                     {"Value": 20,"Points": [
 					 {"X":10,"Y":60},
 					 {"X":10,"Y":90},
 					 {"X":40,"Y":90},
@@ -22,7 +22,7 @@ export class TestScatterShapeData3 extends Array<object>
 					 {"X":60,"Y":60},
 					 {"X":60,"Y":90},
 					 {"X":90,"Y":90},
-					 {"X":90,"Y":60}]]}];
+					 {"X":90,"Y":60}]}];
                  
             super(...newItems.slice(0));
         }
