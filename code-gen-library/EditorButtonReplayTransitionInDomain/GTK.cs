7d49a3291933f5a1e9c219763345af96053ee21e
gmurray81 <gmurray@gmail.com>
2026-03-20 17:42:33 -0400

    gtk library items
    Co-authored-by: gmurray81 <4264873+gmurray81@users.noreply.github.com>

---
+++ code-gen-library/EditorButtonReplayTransitionInDomain/GTK.cs
@@ -0,0 +1,22 @@
+//begin imports
+using Infragistics.Controls;
+using Infragistics.Controls.Description;
+using Infragistics.Controls.Layouts;
+using Infragistics.Controls.Charts;
+//end imports
+
+
+
+public class EditorButtonReplayTransitionInDomain
+{
+
+    //begin eventHandler
+    //WPF: Infragistics.Controls.Layouts.PropertyEditorPropertyDescriptionButtonClickEventHandler
+    public void EditorButtonReplayTransitionInDomain(object sender, PropertyEditorPropertyDescriptionButtonClickEventArgs args)
+    {
+        var chart = CodeGenHelper.GetDescription<DomainChart>("content");
+        chart.ReplayTransitionIn();
+        
+    }
+    //end eventHandler
+}
