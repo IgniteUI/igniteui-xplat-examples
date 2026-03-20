a8de5d669798297226a619e79cd7668abce9712d
gmurray81 <gmurray@gmail.com>
2026-03-20 17:42:33 -0400

    gtk library items

---
+++ code-gen-library/EditorChangeUpdateValueLines/GTK.cs
@@ -0,0 +1,25 @@
+//begin imports
+using Infragistics.Controls;
+using Infragistics.Controls.Description;
+using Infragistics.Controls.Layouts;
+using Infragistics.Controls.Charts;
+using System;
+//end imports
+
+public class EditorChangeUpdateValueLines
+{
+
+    //begin eventHandler
+    //WPF: Infragistics.Controls.Layouts.PropertyEditorPropertyDescriptionChangedEventHandler
+    public void EditorChangeUpdateValueLines(object sender, PropertyEditorPropertyDescriptionChangedEventArgs args)
+    {
+        var item = (PropertyEditorPropertyDescription)sender;
+        var value = (string)item.PrimitiveValue;
+        var chart = CodeGenHelper.GetDescription<XamCategoryChart>("content");
+       
+        var valueLineType = (ValueLayerValueMode)Enum.Parse(typeof(ValueLayerValueMode), value);
+        chart.ValueLines.Clear();
+        chart.ValueLines.Add(valueLineType);
+    }
+    //end eventHandler
+}
