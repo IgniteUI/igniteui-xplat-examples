1ab3f8e0d51cda8b98668a181ef606f229959f95
gmurray81 <gmurray@gmail.com>
2026-03-20 17:42:33 -0400

    gtk library items
    Co-authored-by: gmurray81 <4264873+gmurray81@users.noreply.github.com>

---
+++ code-gen-library/FormatDateLabelAsTime/GTK.cs
@@ -0,0 +1,47 @@
+//begin imports
+using Infragistics.Controls.Charts;
+using System;
+using System.Collections;
+//end imports
+
+public class FormatDateLabelAsTime
+{
+    //begin eventHandler
+    //WPF: Infragistics.Controls.Charts.AxisFormatLabelEventHandler
+    public void FormatDateLabelAsTime(object sender, AxisFormatLabelEventArgs args)
+    {
+        var item = args.Item;
+        DateTime d;
+        if (item is DateTime)
+        {
+            d = (DateTime)item;
+
+        }
+        else if (item is IDictionary)
+        {
+            d = (DateTime)((IDictionary)item)["Date"];
+        }
+        else
+        {
+            if (item is double)
+            {
+                if (((double)item) >= long.MinValue && ((double)item) <= long.MaxValue)
+                {
+                    double db = (double)item;
+                    long ticks = (long)db;
+                    d = new DateTime(ticks);
+                }
+                else
+                {
+                    args.Label = item.ToString();
+                    return;
+                }
+            }
+            else
+                d = (DateTime)item.GetType().GetProperty("Date").GetValue(item);
+
+        }
+        args.Label = d.ToString("HH:mm:ss");
+    }
+    //end eventHandler
+}
