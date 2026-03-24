fc36e30310ba807065104bb9c6063189a370eb3f
IGvaleries <vsuszynski@infragistics.com>
2026-03-24 11:32:51 -0400

    fix scatter shape data for web

--- code-gen-library/TestScatterShapeData4/Web.ts
+++ code-gen-library/TestScatterShapeData4/Web.ts
@@ -5,16 +5,16 @@
 export class TestScatterShapeData4 extends Array<object>
 {
    public constructor(){
-	    const newItems = [{ "Value": 5,"Points":[
+	    const newItems = [{ "Value": 5,"Points":[[
                      {"X":40,"Y":40},
 					 {"X":40,"Y":60},
 					 {"X":60,"Y":60},
-					 {"X":60,"Y":40}]},              
-                     {"Value": 10,"Points": [
+					 {"X":60,"Y":40}]]},              
+                     {"Value": 10,"Points": [[
 					 {"X":50,"Y":50},
 					 {"X":50,"Y":70},
 					 {"X":70,"Y":70},
-					 {"X":70,"Y":50}]}];
+					 {"X":70,"Y":50}]]}];
                  
             super(...newItems.slice(0));
         }
