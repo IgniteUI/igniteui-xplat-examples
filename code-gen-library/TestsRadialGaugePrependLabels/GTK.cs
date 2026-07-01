c89e6a586790bf88fed47bfb185ab879a8c8b473
gmurray81 <gmurray@gmail.com>
2026-06-30 21:39:42 -0400

    fix gtk tests

---
+++ code-gen-library/TestsRadialGaugePrependLabels/GTK.cs
@@ -0,0 +1,19 @@
+//begin imports
+using Infragistics.Controls.Gauges;
+//end imports
+
+public class TestsRadialGaugePrependLabels
+{
+    //begin eventHandler
+	//WPF: Infragistics.Controls.Gauges.FormatRadialGaugeLabelHandler
+    public void TestsRadialGaugePrependLabels(object sender, FormatRadialGaugeLabelEventArgs args)
+    {													
+		object o = CodeGenHelper.FindByName<object>("LabelPrependValue");
+		var parser = new JsonDictionaryParser();
+		var obj = (JsonDictionaryObject)parser.Parse((string)((JsonDictionaryValue)o).Value);
+		var v = (string)(obj["Text"] as JsonDictionaryValue).Value;
+		args.Label = v + args.Value;
+			
+    }
+    //end eventHandler
+}
